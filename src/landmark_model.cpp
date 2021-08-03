fd59ed21e128ee144756bb7f650a6b51a750f7e2
Sona Molnarova <molnsona@gmail.com>
2021-08-03 17:43:45 +0200

    Try to fix highdim_vertices in update in landmark model.

--- src/landmark_model.cpp
+++ src/landmark_model.cpp
@@ -45,12 +45,17 @@ LandmarkModel::update(const DataModel &data)
 
     hidim_vertices.resize(n * n * d);
 
-    for (size_t i = 0; i < n * n; ++i) {
-        auto x = i % n;
-        auto y = i / n;
+    std::size_t index = 0;
+    for (size_t i = 100; i < 500; i += 400/n) {
+        for (int j = -150; j < 150; j += 300 / n) {
+            float y = i;
+            float z = j;
 
-        // TODO compute this dynamically from data model
-        hidim_vertices[d * i + 0] = x / float(n);
-        hidim_vertices[d * i + 1] = y / float(n);
+            // TODO compute this dynamically from data model
+            hidim_vertices[d * index + 0] = 0;
+            hidim_vertices[d * index + 1] = y;
+            hidim_vertices[d * index + 2] = z;
+            ++index;
+        }
     }
 }
