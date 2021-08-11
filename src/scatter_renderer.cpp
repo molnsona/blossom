e606c3800baaf57a53bad84e09226007adf41d89
Sona Molnarova <molnsona@gmail.com>
2021-08-11 21:19:10 +0200

    Add color bufferto points.

--- src/scatter_renderer.cpp
+++ src/scatter_renderer.cpp
@@ -2,6 +2,7 @@
 #include "scatter_renderer.h"
 
 #include <Magnum/Math/Color.h>
+#include <Magnum/MeshTools/Interleave.h>
 #include <Magnum/Trade/MeshData.h>
 
 using namespace Magnum;
@@ -15,16 +16,19 @@ ScatterRenderer::ScatterRenderer()
 void
 ScatterRenderer::draw(const View &view, const ScatterModel &model)
 {
+    std::vector<Color3> color(model.points.size(), 0x000000_rgbf);
     GL::Buffer buffer;
-    buffer.setData(
-      Corrade::Containers::ArrayView(model.points.data(), model.points.size()));
+    buffer.setData(MeshTools::interleave(
+      Corrade::Containers::ArrayView(model.points.data(), model.points.size()),
+      Corrade::Containers::ArrayView(color.data(), color.size())));
 
     point_mesh.setCount(model.points.size())
-      .addVertexBuffer(std::move(buffer), 0, decltype(flat_shader)::Position{});
+      .addVertexBuffer(std::move(buffer),
+                       0,
+                       decltype(flat_shader)::Position{},
+                       decltype(flat_shader)::Color3{});
 
     auto proj = view.projection_matrix();
 
-    flat_shader.setColor(0x88ccff_rgbf)
-      .setTransformationProjectionMatrix(proj)
-      .draw(point_mesh);
+    flat_shader.setTransformationProjectionMatrix(proj).draw(point_mesh);
 }
