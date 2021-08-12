315f8557634d88697ae3496cb5b7e9dbc814e208
Sona Molnarova <molnsona@gmail.com>
2021-08-12 11:36:48 +0200

    Color points according to second parameter.

--- src/scatter_renderer.cpp
+++ src/scatter_renderer.cpp
@@ -1,9 +1,16 @@
 
+#include <extern/colormap/color.hpp>
+#include <extern/colormap/map.hpp>
+#include <extern/colormap/palettes.hpp>
+#include <extern/colormap/pixmap.hpp>
+
 #include "scatter_renderer.h"
 
-#include <Magnum/Math/Color.h>
 #include <Magnum/MeshTools/Interleave.h>
 #include <Magnum/Trade/MeshData.h>
+
+#include <algorithm>
+#include <limits>
 
 using namespace Magnum;
 using namespace Math::Literals;
@@ -14,9 +21,11 @@ ScatterRenderer::ScatterRenderer()
 }
 
 void
-ScatterRenderer::draw(const View &view, const ScatterModel &model)
+ScatterRenderer::draw(const View &view,
+                      const ScatterModel &model,
+                      const TransData &trans_data)
 {
-    std::vector<Color3> color(model.points.size(), 0x000000_rgbf);
+    std::vector<Color3> color = fill_color(trans_data);
     GL::Buffer buffer;
     buffer.setData(MeshTools::interleave(
       Corrade::Containers::ArrayView(model.points.data(), model.points.size()),
@@ -31,4 +40,30 @@ ScatterRenderer::draw(const View &view, const ScatterModel &model)
     auto proj = view.projection_matrix();
 
     flat_shader.setTransformationProjectionMatrix(proj).draw(point_mesh);
+}
+
+std::vector<Color3>
+ScatterRenderer::fill_color(const TransData &trans_data)
+{
+    std::vector<Color3> color(trans_data.n);
+
+    float min{ std::numeric_limits<float>::max() },
+      max{ std::numeric_limits<float>::min() };
+
+    for (size_t i = 0; i < trans_data.n; ++i) {
+        // take second parameter
+        max = std::max(max, trans_data.get_data().at(i * trans_data.d + 1));
+        min = std::min(min, trans_data.get_data().at(i * trans_data.d + 1));
+    }
+
+    auto col_palette = colormap::palettes.at("rdbu").rescale(min, max);
+
+    for (size_t i = 0; i < trans_data.n; ++i) {
+        auto c = col_palette(trans_data.get_data().at(i * trans_data.d + 1));
+        std::vector<unsigned char> res;
+        c.get_rgb(res);
+        color[i] = Color3(res[0] / 255.0f, res[1] / 255.0f, res[2] / 255.0f);
+    }
+
+    return color;
 }
--- src/scatter_renderer.h
+++ src/scatter_renderer.h
@@ -3,20 +3,31 @@
 #define SCATTER_RENDERER_H
 
 #include <Magnum/GL/Mesh.h>
+#include <Magnum/Math/Color.h>
 #include <Magnum/Shaders/FlatGL.h>
 
 #include "scatter_model.h"
+#include "trans_data.h"
 #include "view.h"
+
+using namespace Magnum;
+using namespace Math::Literals;
 
 struct ScatterRenderer
 {
     ScatterRenderer();
 
-    void draw(const View &v, const ScatterModel &m);
+    void draw(const View &v,
+              const ScatterModel &m,
+              const TransData &trans_data);
 
 private:
+    std::vector<Color3> fill_color(const TransData &trans_data);
+
     Magnum::GL::Mesh point_mesh;
-    Magnum::Shaders::FlatGL2D flat_shader;
+    Magnum::Shaders::FlatGL2D flat_shader{
+        Shaders::FlatGL2D::Flag::VertexColor
+    };
 };
 
 #endif
