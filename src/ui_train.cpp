61933bac5da53c012023cdea429b19a4ca028afd
Sona Molnarova <molnsona@gmail.com>
2021-11-26 23:05:09 +0100

    Fix missing ImGui::Ends.

--- src/ui_train.cpp
+++ src/ui_train.cpp
@@ -12,59 +12,65 @@ uiTrainingSettings::render(Application &app, ImGuiWindowFlags window_flags)
     if (!show_window)
         return;
 
-    if (!ImGui::Begin("Training settings", &show_window, window_flags))
-        return;
+    if (ImGui::Begin("Training settings", &show_window, window_flags)) {
 
-    ImGui::SliderInt(
-      "Iterations (SOM and k-means)", &app.state.training_conf.iters, 0, 200);
-    ImGui::SliderFloat("Alpha (SOM and k-means)", // TODO: logarithmicly
-                       &app.state.training_conf.alpha,
-                       0.001f,
-                       2.0f,
-                       "%.3f",
-                       ImGuiSliderFlags_AlwaysClamp);
+        ImGui::SliderInt("Iterations (SOM and k-means)",
+                         &app.state.training_conf.iters,
+                         0,
+                         200);
+        ImGui::SliderFloat("Alpha (SOM and k-means)", // TODO: logarithmicly
+                           &app.state.training_conf.alpha,
+                           0.001f,
+                           2.0f,
+                           "%.3f",
+                           ImGuiSliderFlags_AlwaysClamp);
 
-    ImGui::Checkbox("SOM", &app.state.training_conf.som_landmark);
-    ImGui::SliderFloat("Sigma", // TODO: odmocninovo
-                       &app.state.training_conf.sigma,
-                       0.1f,
-                       5.0f,
-                       "%.3f",
-                       ImGuiSliderFlags_AlwaysClamp);
+        ImGui::Checkbox("SOM", &app.state.training_conf.som_landmark);
+        ImGui::SliderFloat("Sigma", // TODO: odmocninovo
+                           &app.state.training_conf.sigma,
+                           0.1f,
+                           5.0f,
+                           "%.3f",
+                           ImGuiSliderFlags_AlwaysClamp);
 
-    ImGui::Checkbox("k-means", &app.state.training_conf.kmeans_landmark);
-    ImGui::SliderFloat("Gravity", // TODO: exponencialne
-                       &app.state.training_conf.gravity,
-                       0.0f,
-                       0.1f,
-                       "%.3f",
-                       ImGuiSliderFlags_AlwaysClamp);
+        ImGui::Checkbox("k-means", &app.state.training_conf.kmeans_landmark);
+        ImGui::SliderFloat("Gravity", // TODO: exponencialne
+                           &app.state.training_conf.gravity,
+                           0.0f,
+                           0.1f,
+                           "%.3f",
+                           ImGuiSliderFlags_AlwaysClamp);
 
-    ImGui::Checkbox("Generate k-NN graph", &app.state.training_conf.knn_edges);
-    ImGui::SliderInt(
-      "Graph neighbors (k)", &app.state.training_conf.kns, 0, 10);
-    ImGui::Checkbox("Layout the graph", &app.state.training_conf.graph_layout);
+        ImGui::Checkbox("Generate k-NN graph",
+                        &app.state.training_conf.knn_edges);
+        ImGui::SliderInt(
+          "Graph neighbors (k)", &app.state.training_conf.kns, 0, 10);
+        ImGui::Checkbox("Layout the graph",
+                        &app.state.training_conf.graph_layout);
 
-    // TODO: show parameters according to active training method
+        // TODO: show parameters according to active training method
 
-    ImGui::Text("EmbedSOM settings");
+        ImGui::Text("EmbedSOM settings");
 
-    ImGui::SliderInt(
-      "k (landmark neighborhood size)", &app.state.training_conf.topn, 3, 32);
+        ImGui::SliderInt("k (landmark neighborhood size)",
+                         &app.state.training_conf.topn,
+                         3,
+                         32);
 
-    ImGui::SliderFloat("Boost", // TODO: use smooth instead
-                       &app.state.training_conf.boost,
-                       0.2f,
-                       5.0f,
-                       "%.3f",
-                       ImGuiSliderFlags_AlwaysClamp);
+        ImGui::SliderFloat("Boost", // TODO: use smooth instead
+                           &app.state.training_conf.boost,
+                           0.2f,
+                           5.0f,
+                           "%.3f",
+                           ImGuiSliderFlags_AlwaysClamp);
 
-    ImGui::SliderFloat("Adjust",
-                       &app.state.training_conf.adjust,
-                       0.0f,
-                       2.0f,
-                       "%.3f",
-                       ImGuiSliderFlags_AlwaysClamp);
+        ImGui::SliderFloat("Adjust",
+                           &app.state.training_conf.adjust,
+                           0.0f,
+                           2.0f,
+                           "%.3f",
+                           ImGuiSliderFlags_AlwaysClamp);
 
-    ImGui::End();
+        ImGui::End();
+    }
 }
