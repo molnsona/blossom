86c06c45a3c3f4be23d8e5589015967ea69a867c
Sona Molnarova <molnsona@gmail.com>
2022-09-16 21:47:59 +0200

    Basic GLFW window.

--- src/style.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd `dirname $0` #lol
-clang-format -style="{BasedOnStyle: Mozilla, UseTab: Never, IndentWidth: 4, TabWidth: 4, AccessModifierOffset: -4, PointerAlignment: Right}" -verbose -i *.cpp *.h *.hpp *.cu *.cuh
