2f80f6741b75be19c729e479f4e4f8456923ca8b
Sona Molnarova <molnsona@gmail.com>
2022-09-18 13:54:10 +0200

    Basic glfw window and imgui demo works.

--- src/app.cpp
+++ src/app.cpp
@@ -1,42 +1,74 @@
 
+#define GLFW_INCLUDE_NONE
+
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
+
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 int main()
 {
-    GLFWwindow* window;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "BlosSOM", NULL, NULL);
+    auto window = glfwCreateWindow(800, 600, "Example", nullptr, nullptr);
     if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
+        throw std::runtime_error("Error creating glfw window");
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    if (!gladLoaderLoadGL())
+        throw std::runtime_error("Error initializing glad");
+
+    /**
+     * Initialize ImGui
+     */
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 450 core");
+    ImGui::StyleColorsClassic();
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEBUG_OUTPUT);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        static bool showDemo = false;
+        ImGui::Begin("Example");
+        if (ImGui::Button("Show/Hide ImGui demo"))
+        showDemo = !showDemo;
+        ImGui::End();
+        if (showDemo)
+        ImGui::ShowDemoWindow(&showDemo);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
